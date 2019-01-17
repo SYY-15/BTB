8b0948e953088bb24349f7b1f67a12800f377a45
SYY-15 <2472814735@qq.com>
2019-01-17 20:42:21 +0800

    主页修改

--- src/main/webapp/com/btb/user/js/login.js
+++ src/main/webapp/com/btb/user/js/login.js
@@ -2,24 +2,26 @@
  * 登录页面脚本
  */
 $(document).ready(function() {
-		$("#dl").click(function() {
-			var data = $("form").serialize();
-			$.ajax({
-				type : 'post',
-				url : "/BTB/LoginServlet",
-				cache : false,
-				data : data,
-				dataType : 'json',
-				success : function(data) {
-					alert(data.msg);
-					if (data.status == 1) {
-						window.location.href = "../../index/html/index.html"
-					} 
-				},
-				error : function() {
-					alert("请求失败")
-				}
-			})
-		});
+	$("#dl").click(function() {
+		var data = $("form").serialize();
+		$.ajax({
+			type : 'post',
+			url : "/BTB/LoginServlet",
+			cache : false,
+			data : data,
+			dataType : 'json',
+			success : function(data) {
 
+				if (data.status == 1) {
+					window.location.href = "../../index/html/index.html"
+				} else {
+					alert(data.msg);
+				}
+			},
+			error : function() {
+				alert("请求失败")
+			}
+		})
 	});
+
+});
