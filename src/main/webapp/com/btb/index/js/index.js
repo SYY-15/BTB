e53042b34945e5107354f9b9631a44293d3c9931
SYY-15 <2472814735@qq.com>
2019-01-17 22:04:06 +0800

    index.js

--- src/main/webapp/com/btb/index/js/index.js
+++ src/main/webapp/com/btb/index/js/index.js
@@ -1,5 +1,21 @@
 /**
  * 
  */
+$(document).ready(function() {
+	
+$("#div1").load("/ProductSelvlet",function(response,status){
+	if(status=='success'){
+		alert("请求成功");
+		alert(response.producturl);
+		"<td><img src="+response.productrul+"/></td>"
+		
+	}else{
+		
+		alert("请求失败");
+		
+	}
 
-
+	
+	
+})
+});
