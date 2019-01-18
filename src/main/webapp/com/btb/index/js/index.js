dcd32ca2d7282d86d1b2b78b93933aeeca094b0d
SYY-15 <2472814735@qq.com>
2019-01-18 23:25:28 +0800

    主页修改

--- src/main/webapp/com/btb/index/js/index.js
+++ src/main/webapp/com/btb/index/js/index.js
@@ -1,23 +1,68 @@
 /**
  * 
  */
+var ctxPath = window.location.origin + "/"
+					+ window.location.pathname.split("/")[1];
+var tableData;
 $(document).ready(
 		function() {
-			var ctxPath = window.location.origin + "/"
-					+ window.location.pathname.split("/")[1];
+			
 			$.getJSON(ctxPath + "/ProductServlet", function(data) {
 				var tr = $("<tr></tr>");
+				tableData = data.data;
 				$.each(data.data, function(i, data) {
-					if (i%4 == 0) {
+					var td = $("<td></td>");
+					if (i % 4 == 0 && i != 0) {
 						$("#table1").append(tr);
 						tr = $("<tr></tr>");
 					}
-					var div = $("<div></div>");
-					$(div).append("<img src=\""+ctxPath+data.prodoctPicture+"\"/>");
-					$(div).append("<a href=\""+ctxPath+data.prodoctUrl+"\"/>"+ctxPath+data.prodoctName+"</p>");
-					$(div).append("<p>"+ctxPath+data.prodoctPrice+"</p>");
-					$(tr).append(div);
+					var ul = $("<ul></ul>");
+					$(ul).append(
+							"<li><a href=\"" + ctxPath + data.prodoctUrl
+									+ "\"><img src=\"" + ctxPath
+									+ data.prodoctPicture + "\"/></a></li>");
+					$(ul).append("<li>￥" + data.prodoctPrice + "</li>");
+					$(ul).append(
+							"<li><a href=\"" + ctxPath + data.prodoctUrl
+									+ "\">" + data.prodoctName + "</a></li>");
+					$(td).append(ul);
+					$(tr).append(td);
 				})
 				$("#table1").append(tr);
+
 			})
+
 		});
+$(window).resize(function() {
+	var size = 4;
+	console.log(document.body.clientWidth);
+	
+	if(document.body.clientWidth < 1000){
+		size=3;
+	}else if(document.body.clientWidth <= 1366){
+		size=4;
+	}else{
+		size=5;
+	}
+	var tr = $("<tr></tr>");
+	$("#table1").empty();
+	$.each(tableData, function(i, data) {
+		var td = $("<td></td>");
+		if (i % size == 0 && i != 0) {
+			$("#table1").append(tr);
+			tr = $("<tr></tr>");
+		}
+		var ul = $("<ul></ul>");
+		$(ul).append(
+				"<li><a href=\"" + ctxPath + data.prodoctUrl
+						+ "\"><img src=\"" + ctxPath
+						+ data.prodoctPicture + "\"/></a></li>");
+		$(ul).append("<li>￥" + data.prodoctPrice + "</li>");
+		$(ul).append(
+				"<li><a href=\"" + ctxPath + data.prodoctUrl
+						+ "\">" + data.prodoctName + "</a></li>");
+		$(td).append(ul);
+		$(tr).append(td);
+	})
+	$("#table1").append(tr);
+});
